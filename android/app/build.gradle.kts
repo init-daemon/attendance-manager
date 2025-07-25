741625e1084265768d2243b769662c05064da3f3
Rxon <37933415+init-daemon@users.noreply.github.com>
2025-07-25 04:08:30 +0000

    chore(package): update package (#21)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.daemon.init.attendanceapp"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973" // Modification ici
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
